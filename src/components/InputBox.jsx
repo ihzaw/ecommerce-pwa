61541853ff8f905e2a615d8d8d38a1c4e6115da7
ihzaw <ihzanantama@gmail.com>
2022-03-06 16:47:20 +0800

    add badge component, add badge styling responsivity

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,15 +1,19 @@
 import { Mail } from "../components/Mail";
-import { Lock } from '../components/Lock'
+import { Lock } from "../components/Lock";
 
 export const InputBox = ({ type }) => {
   return (
-    <label className="flex items-center relative">
-      <div className="flex items-center text-grey-primary font-dmsans absolute left-4">
-        <div className="w-auto h-auto mr-[24.17px]">
+    <label className="relative flex items-center">
+      <div className="absolute left-4 flex items-center font-dmsans text-grey-primary">
+        <div className="mr-[24.17px] h-auto w-auto">
           {type === "Email" ? <Mail /> : <Lock />}
         </div>
       </div>
-      <input type={type === "Email" ? "text" : "password"} className="bg-white h-[50px] rounded-[10px] w-full outline-none py-[15px] px-16" placeholder={type === "Email" ? "Email" : "Password"}/>
+      <input
+        type={type === "Email" ? "text" : "password"}
+        className="h-[50px] w-full rounded-[10px] bg-white py-[15px] px-16 outline-none"
+        placeholder={type === "Email" ? "Email" : "Password"}
+      />
     </label>
   );
 };
